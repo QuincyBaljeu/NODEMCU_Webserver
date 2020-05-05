400e9d4929c1d6b2d2ef6787406bc370435efdd4
Quincy <q.baljeu@gmail.com>
2020-05-05 21:35:35 +0200

    Added wifi connection

--- NodeMCU_Webserver/NodeMCU_Webserver.ino
+++ NodeMCU_Webserver/NodeMCU_Webserver.ino
@@ -1,23 +1,35 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include "DHT.h"
 
 #define DHT11_PIN D1
+
+#define SSID "Haagdijk 43938C 2.4GHz"
+#define PASSWORD "vier rode fietsende olifanten"
 
 DHT dht11(DHT11_PIN, DHT11);
  
 void setup() {
   Serial.begin(115200);
 
+  //Setup for DHT11 Temp/Humi sensor
   pinMode(DHT11_PIN, INPUT);
   dht11.begin();
+
+  //Setup for WiFi connection
+  Serial.print("Conecting to ssid: ");
+  Serial.print(SSID);
+
+  WiFi.begin(SSID, PASSWORD);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi Connected!");
 }
 
 
 void loop() {
-
-  Serial.print("TEMP");
-  Serial.print(dht11.readTemperature());
-  Serial.println();
-  Serial.print("HUM");
-  Serial.print(dht11.readHumidity());
-  delay(1000);
 }
